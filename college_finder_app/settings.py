11ec6baeaddb83e283c52018bd7d284770e08585
Madan Neupane <madanneupane09@gmail.com>
2025-05-30 18:32:50 +0545

    fix: enable WhiteNoise static files storage for improved asset management

--- college_finder_app/settings.py
+++ college_finder_app/settings.py
@@ -214,7 +214,7 @@ if not DEBUG:
 
     # Static files
     # STATICFILES_STORAGE = 'college_finder_app.storage.WhiteNoiseStaticFilesStorage'
-    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
