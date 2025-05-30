5edf1f92b748749261f0c5399b661db3a07d3631
Madan Neupane <madanneupane09@gmail.com>
2025-05-30 18:56:13 +0545

    fix(build): ensure data fixtures are loaded after migrations

--- build.sh
+++ build.sh
@@ -11,3 +11,6 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py makemigrations
 python manage.py migrate
+
+# Run fixtures from data.json
+python manage.py loaddata data.json
